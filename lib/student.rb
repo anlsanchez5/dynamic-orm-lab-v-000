8a32fb2ceb370e11d16fd1d0124ffab7d371e957
Angelica Sanchez <anlsanchez@ucdavis.edu>
2019-03-06 20:45:35 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -4,9 +4,9 @@ require 'active_support/inflector'
 require 'interactive_record.rb'
 
 class Student < InteractiveRecord
-  self.column_names.delete_if {|col| (col.is_a? Integer) == true}
+  col_names = self.column_names.delete_if {|col| (col.is_a? Integer) == true}
   binding.pry
-  self.column_names.each do |col_name|
+  col_names.each do |col_name|
     attr_accessor col_name.to_sym
   end
 
