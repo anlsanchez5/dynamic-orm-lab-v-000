235e674b595c509148abfbd8acdbc0fdc7bc339e
Angelica Sanchez <anlsanchez@ucdavis.edu>
2019-03-06 23:05:58 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -57,7 +57,7 @@ class InteractiveRecord
     DB[:conn].execute("SELECT * FROM #{self.table_name} WHERE name = ?",name)
   end
 
-  def self.find_by(options{})
+  def self.find_by(options = {})
     binding.pry
     DB[:conn].execute("SELECT * FROM #{self.table_name} WHERE #{attribute} = ?", value)
   end
