85e296bc95c393c3eb844ae116810cc3e033f9c8
Angelica Sanchez <anlsanchez@ucdavis.edu>
2019-03-06 22:23:15 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -56,6 +56,7 @@ class InteractiveRecord
   def self.find_by_name(name)
     DB[:conn].results_as_hash = true
     sql = DB[:conn].execute("SELECT * FROM #{self.table_name} WHERE name = ? GROUP BY id ORDER BY cid",name)
+    binding.pry
   end
 
   def self.find_by(attribute)
